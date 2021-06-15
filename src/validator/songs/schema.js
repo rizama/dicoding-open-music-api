3e47a54938ee6c7ae7923d5d7717aa82492af4d6
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-06-15 12:09:13 +0700

    ^ review: add min and max year

--- src/validator/songs/schema.js
+++ src/validator/songs/schema.js
@@ -2,7 +2,8 @@ const Joi = require('joi');
 
 const SongPayloadSchema = Joi.object({
     title: Joi.string().required(),
-    year: Joi.number().integer().required(),
+    year: Joi.number().integer().min(1900).max(2030)
+        .required(),
     performer: Joi.string().required(),
     genre: Joi.string().required(),
     duration: Joi.number().integer().required(),
