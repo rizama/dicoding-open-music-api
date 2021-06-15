b3edc2007821a5a858d541680d73b2499d939c0a
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-06-15 12:09:01 +0700

    ^ add function error

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,24 @@
 /* eslint-disable object-curly-newline */
 /* eslint-disable camelcase */
+
+const ClientError = require('../exceptions/ClientError');
+
 const mapDBToModel = ({ id, title, performer }) => ({
     id,
     title,
     performer,
 });
 
-const mapDBToModelDetail = ({ id, title, year, performer, genre, duration, created_at, updated_at }) => ({
+const mapDBToModelDetail = ({
+    id,
+    title,
+    year,
+    performer,
+    genre,
+    duration,
+    created_at,
+    updated_at,
+}) => ({
     id,
     title,
     year,
@@ -17,4 +29,25 @@ const mapDBToModelDetail = ({ id, title, year, performer, genre, duration, creat
     updatedAt: updated_at,
 });
 
-module.exports = { mapDBToModel, mapDBToModelDetail };
+const errorHandler = (error, h) => {
+    if (error instanceof ClientError) {
+        const response = h.response({
+            status: 'fail',
+            message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+    }
+
+    // Server ERROR!
+    const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+    });
+    response.code(500);
+    console.error(error);
+
+    return response;
+};
+
+module.exports = { mapDBToModel, mapDBToModelDetail, errorHandler };
