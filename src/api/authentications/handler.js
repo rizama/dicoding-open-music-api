3d730bc02ec62f3d2727fea3f4799499a02e7be5
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-06-26 23:05:00 +0700

    ^ Authentications login, refresh, and logout (delete)

---
+++ src/api/authentications/handler.js
@@ -0,0 +1,91 @@
+const { errorHandler } = require('../../utils');
+
+class AuthenticationsHandler {
+    constructor(authenticationsService, usersService, tokenManager, validator) {
+        this._authenticationsService = authenticationsService;
+        this._usersService = usersService;
+        this._tokenManager = tokenManager;
+        this._validator = validator;
+
+        this.postAuthenticationHandler = this.postAuthenticationHandler.bind(this);
+        this.putAuthenticationHandler = this.putAuthenticationHandler.bind(this);
+        this.deleteAuthenticationHandler = this.deleteAuthenticationHandler.bind(this);
+    }
+
+    async postAuthenticationHandler(request, h) {
+        try {
+            this._validator.validatePostAuthenticationPayload(request.payload);
+
+            const { username, password } = request.payload;
+            const id = await this._usersService.verifyUserCredential(
+                username,
+                password
+            );
+
+            const accessToken = this._tokenManager.generateAccessToken({ id });
+            const refreshToken = this._tokenManager.generateRefreshToken({
+                id,
+            });
+
+            await this._authenticationsService.addRefreshToken(refreshToken);
+
+            const response = h.response({
+                status: 'success',
+                message: 'Authentication berhasil ditambahkan',
+                data: {
+                    accessToken,
+                    refreshToken,
+                },
+            });
+            response.code(201);
+            return response;
+        } catch (error) {
+            return errorHandler(error, h);
+        }
+    }
+
+    async putAuthenticationHandler(request, h) {
+        try {
+            this._validator.validatePutAuthenticationPayload(request.payload);
+
+            const { refreshToken } = request.payload;
+
+            await this._authenticationsService.verifyRefreshToken(refreshToken);
+            const { id } = this._tokenManager.verifyRefreshToken(refreshToken);
+
+            const accessToken = this._tokenManager.generateAccessToken({ id });
+
+            return {
+                status: 'success',
+                message: 'Access Token berhasil diperbarui',
+                data: {
+                    accessToken,
+                },
+            };
+        } catch (error) {
+            return errorHandler(error, h);
+        }
+    }
+
+    async deleteAuthenticationHandler(request, h) {
+        try {
+            this._validator.validateDeleteAuthenticationPayload(
+                request.payload
+            );
+
+            const { refreshToken } = request.payload;
+
+            await this._authenticationsService.verifyRefreshToken(refreshToken);
+            await this._authenticationsService.deleteRefreshToken(refreshToken);
+
+            return {
+                status: 'success',
+                message: 'Refresh token berhasil dihapus',
+            };
+        } catch (error) {
+            return errorHandler(error, h);
+        }
+    }
+}
+
+module.exports = AuthenticationsHandler;
--- src/server.js
+++ src/server.js
@@ -9,9 +9,15 @@ const users = require('./api/users');
 const UsersService = require('./services/postgres/UsersService');
 const UsersValidator = require('./validator/users');
 
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
+const AuthenticationsValidator = require('./validator/authentications');
+const TokenManager = require('./tokenize/TokenManager');
+
 const init = async () => {
     const songsService = new SongsService();
     const usersService = new UsersService();
+    const authenticationsService = new AuthenticationsService();
 
     const server = Hapi.server({
         port: process.env.PORT,
@@ -36,6 +42,15 @@ const init = async () => {
             options: {
                 service: usersService,
                 validator: UsersValidator,
+            },
+        },
+        {
+            plugin: authentications,
+            options: {
+                authenticationsService,
+                usersService,
+                tokenManager: TokenManager,
+                validator: AuthenticationsValidator,
             },
         },
     ]);
