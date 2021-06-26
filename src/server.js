09717887e7fd071cbac7730e6e9610b44aba1e13
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-06-26 16:16:37 +0700

    + service, plugins, validator Users

--- src/server.js
+++ src/server.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
+
 const songs = require('./api/songs');
 const SongsService = require('./services/postgres/SongsService');
 const SongsValidator = require('./validator/songs');
 
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+
 const init = async () => {
     const songsService = new SongsService();
+    const usersService = new UsersService();
 
     const server = Hapi.server({
         port: process.env.PORT,
@@ -17,13 +23,22 @@ const init = async () => {
         },
     });
 
-    await server.register({
-        plugin: songs,
-        options: {
-            service: songsService,
-            validator: SongsValidator,
+    await server.register([
+        {
+            plugin: songs,
+            options: {
+                service: songsService,
+                validator: SongsValidator,
+            },
         },
-    });
+        {
+            plugin: users,
+            options: {
+                service: usersService,
+                validator: UsersValidator,
+            },
+        },
+    ]);
 
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
