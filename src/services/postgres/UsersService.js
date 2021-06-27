223500d3aaccaa4996de9966f76a9b6020e2cb05
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-06-27 21:25:40 +0700

    ^ add serach by username

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -81,6 +81,15 @@ class UsersService {
 
         return id;
     }
+
+    async getUsersByUsername(username) {
+        const query = {
+            text: 'SELECT id, username, fullname FROM users WHERE username LIKE $1',
+            values: [`%${username}%`],
+        };
+        const result = await this._pool.query(query);
+        return result.rows;
+    }
 }
 
 module.exports = UsersService;
