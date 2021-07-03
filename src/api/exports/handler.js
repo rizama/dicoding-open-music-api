6e3774937b7426def19d1449851f1ff770cf5ee9
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-07-03 23:49:19 +0700

    + plugins exports

---
+++ src/api/exports/handler.js
@@ -0,0 +1,45 @@
+const { errorHandler } = require('../../utils');
+
+class ExportsHandler {
+    constructor(service, validator, playlistsService) {
+        this._service = service;
+        this._validator = validator;
+        this._playlistsService = playlistsService;
+
+        this.postExportPlaylistsHandler =
+            this.postExportPlaylistsHandler.bind(this);
+    }
+
+    async postExportPlaylistsHandler(request, h) {
+        try {
+            this._validator.validateExportSongsPayload(request.payload);
+
+            const { playlistId } = request.params;
+            const { id: userId } = request.auth.credentials;
+
+            await this._playlistsService.verifyPlaylistAccess(playlistId, userId);
+
+            const message = {
+                playlistId,
+                targetEmail: request.payload.targetEmail,
+            };
+
+            await this._service.sendMessage(
+                'export:songs',
+                JSON.stringify(message)
+            );
+
+            const response = h.response({
+                status: 'success',
+                message: 'Permintaan Anda sedang kami proses',
+            });
+
+            response.code(201);
+            return response;
+        } catch (error) {
+            return errorHandler(error, h);
+        }
+    }
+}
+
+module.exports = ExportsHandler;
